0feb4a26d027825f7a8ce598db23eb6bb1881bc7
StepanFriedl <stepanfriedl13@gmail.com>
2024-01-05 17:18:12 +0100

    Initial Commit

---
+++ TransferProtocols2/TransferProtocols2App.swift
@@ -0,0 +1,17 @@
+//
+//  TransferProtocols2App.swift
+//  TransferProtocols2
+//
+//  Created by Štěpán Friedl on 05.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct TransferProtocols2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
